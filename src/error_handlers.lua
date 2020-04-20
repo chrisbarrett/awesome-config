1a5281837544e21ced42302359fa1d40fb50ccc7
Chris Barrett <chris@walrus.cool>
2020-04-20 22:32:59 +1200

    Clean up error handlers

--- src/error_handlers.lua
+++ src/error_handlers.lua
@@ -5,30 +5,32 @@ local naughty = require("naughty")
 
 local in_error = false
 
-local function open_in_emacs(err, notification)
+local function open_in_emacs(err)
   local path = "/tmp/awesome-error.stacktrace"
   local file = io.open(path, 'w')
   file:write(tostring(err))
   file:close()
 
-  awful.spawn({ "emacsclient", "--alternate-editor=emacs",
-                "--eval", '(with-current-buffer (find-file "' .. path  .. '") (compilation-minor-mode))' })
+  local sexpr = '(with-current-buffer (find-file "' .. path  .. '") (compilation-minor-mode))'
+  awful.spawn {"emacsclient", "--alternate-editor=emacs", "--eval", sexpr}
 end
 
 local function notify_error(title, err)
   -- Make sure we don't go into an endless error loop
-  if in_error then return end
+  if in_error then
+    return
+  end
   in_error = true
 
-  naughty.notify({
-      preset = naughty.config.presets.critical,
-      title = title,
-      text = "Click to open the backtrace in Emacs.",
-      run = function(notification)
-        open_in_emacs(err)
-        naughty.destroy(notification, naughty.notificationClosedReason.dismissedByUser)
-      end,
-  })
+  naughty.notify {
+    preset = naughty.config.presets.critical,
+    title = title,
+    text = "Click to open the backtrace in Emacs.",
+    run = function(notification)
+      open_in_emacs(err)
+      naughty.destroy(notification, naughty.notificationClosedReason.dismissedByUser)
+    end,
+  }
 
   in_error = false
 end
@@ -39,7 +41,7 @@ if awesome.startup_errors then
 end
 
 -- Handle runtime errors after startup
-awesome.connect_signal(
-  "debug::error",
-  function (err) notify_error("Error in AwesomeWM configuration", err) end
+awesome.connect_signal("debug::error", function (err)
+    notify_error("Error in AwesomeWM configuration", err)
+  end
 )
